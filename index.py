b412f71faea613cf2fff9c0ca74e5bd20751d6e9
Aamir Sahil <notifysahil@gmail.com>
2024-10-23 20:05:19 +0530

    scrapeyoutube function

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from youtubescraper import YoutubeScraper
 def main():
     url = input("Enter the youtube url")
     youtube_scraper = YoutubeScraper(url)
-    print(youtube_scraper.video_id)
+    
 
 if __name__=="__main__":
     main()
--- youtubescraper/youtubescraper.py
+++ youtubescraper/youtubescraper.py
@@ -1,8 +1,11 @@
 import re
+from dotenv import load_dotenv
+import os
 
 class YoutubeScraper:
-    def __init__(self, url):
-        self.video_id = self._getVideoId(url)
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("YOUTUBE_SCRAPE_KEY")
 
     def _getVideoId(self, url):
         regex = "^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$"
@@ -13,3 +16,26 @@ class YoutubeScraper:
             return match.group(5)  # Returns the video ID
         else:
             return None
+    
+    def _getComments(self):
+        pass
+
+    def _saveComments(self):
+        pass
+
+    def scrapeYoutube(self, url):
+        """
+        Go to youtube url, scrape the comments and save it in save_loc\n
+        Args\n
+            api_key : youtube scrapper api key
+            url : youtube url
+            save_loc : location to save the comments   
+        """
+        # request youtube url
+        save_loc = f"../data/comments_{video_id}.csv"
+
+        video_id = self._getVideoId(url)
+        comments = self._getComments(video_id, self.api_key, save_loc)
+        
+        # save comments
+        self._saveComment(comments, save_loc)
